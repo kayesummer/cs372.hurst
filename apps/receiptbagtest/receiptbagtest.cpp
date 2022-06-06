84b69548d2904c871866f2afc0591f512068ad32
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-06 14:49:01 -0500

    Week3 Commit 21

--- apps/receiptbagtest/receiptbagtest.cpp
+++ apps/receiptbagtest/receiptbagtest.cpp
@@ -13,6 +13,7 @@
 int main()
 {
     ReceiptBag<int>aBagOfIntegers;
+    int uniqueReceipt = 0;
 
     if (aBagOfIntegers.size() == 0)
     {
@@ -33,16 +34,26 @@ int main()
     }
     else 
     {
-        std::cout << "Something went wrong in the insert.() function!" << std::endl;
+        std::cout << "Something went wrong in the .insert() function!" << std::endl;
     }
 
 
     std::cout << "The Size of the Bag: " << aBagOfIntegers.size() << std::endl;      //size behavior
     
-    aBagOfIntegers.insert(9);                                                                               //count behavior
+    std:: cout << uniqueReceipt = aBagOfIntegers.insert(9) << std::endl;     //testing receipt return         //count behavior
     std::cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << std::endl;     //testing w/ duplicate- should be 2
     std::cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << std::endl;    //testing w/o duplicate- should be 1
     std::cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << std::endl;  //testing w/ no instance- should be 0
+
+    aBagOfIntegers.pop();       //pop behavior
+    if (aBagOfIntegers.size() == 10)
+    {
+        std::cout << "There are 10 items in your bag, again." << std::endl;   
+    }
+    else 
+    {
+        std::cout << "Something went wrong in the .pop() function!" << std::endl;
+    }
 
     return 0;
 }
