59769bfee89412436d0214ad07c34470ae7e7e36
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-05 21:15:40 -0500

    Week 3 Commit 9

---
+++ include/receiptbag.h
@@ -0,0 +1,68 @@
+//
+// File:   receiptbag.h
+// Author: Your Glorious Instructor
+// Purpose:
+// Implement the Bag ADT.
+//
+#pragma once
+#include <iostream>
+#include "list.h"
+
+template <typename Thing>
+class ReceiptBag {
+public:
+  ReceiptBag() 
+  {
+    bagContents = new List<Thing*>;
+    bagContents[0] = nullptr;
+  }
+
+  ~ReceiptBag() 
+  {
+    delete [] bagContents;
+    bagContents = nullptr;
+  }
+
+  void insert(Thing aThing) 
+  {
+    bagContents->push_back(&aThing);
+    counter++;
+  }
+
+  void pop() 
+  {
+    Thing* ptrToAThing;
+    int i = bagContents.size();
+    bool thingIsNull = true;
+    while (thingIsNull && (i > 0)) {
+      if (bagContents[i] != nullptr) {
+        ptrToAThing = bagContents[i];
+        thingIsNull = false;
+        ptrToAThing.remove();
+        counter--;
+      }
+    }
+  }
+
+  int size() 
+  {
+    return counter;
+  }
+
+  int count(Thing aThing) 
+  {
+    int numberOfTimes = 0;
+    for (int i = 0; i < counter; i++)
+    {
+      if ((bagContents[i]) == aThing)
+      {
+        numberOfTimes++;
+      }
+    }
+    return numberOfTimes;
+  }
+
+private:
+  List<Thing*> *bagContents;
+  int counter = 0;
+};
