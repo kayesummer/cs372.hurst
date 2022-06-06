63ac16f4c5fe108ed39bc6935511c4a330a9fe21
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-06 14:18:19 -0500

    Week3 Commit 13

---
+++ apps/receiptbagtest/receiptbagtest.cpp
@@ -0,0 +1,48 @@
+//
+// File:   receiptbagtest.cpp
+// Author: Kaylee Hurst
+// Purpose:
+// Test the Receipt Bag ADT.
+//
+
+#include <iostream> 
+#include "receiptbag.h"
+
+
+
+int main()
+{
+    ReceiptBag<int>aBagOfIntegers;
+
+    if (aBagOfIntegers.size() == 0)
+    {
+        std::cout << "Congrats, your bag is currently empty!" << std::endl; //constructor behavior
+    }
+    else
+    {
+        std::cout << "Opps, something went wrong and your bag is not empty!" << std::endl;
+    }
+
+    for (int count = 0; count > 10; count++)    //Insert behavior
+    {
+        aBagOfIntegers.insert(count);
+    }
+    if (aBagOfIntegers.size() == 10)
+    {
+        std::cout << "There are now 10 items in your bag." << std::endl;   
+    }
+    else 
+    {
+        std::cout << "Something went wrong in the insert.() function!" << std::endl;
+    }
+
+
+    std::cout << "The Size of the Bag: " << aBagOfIntegers.size() << std::endl;      //size behavior
+    
+    aBagOfIntegers.insert(9);                                                                               //count behavior
+    std::cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << std::endl;     //testing w/ duplicate- should be 2
+    std::cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << std::endl;    //testing w/o duplicate- should be 1
+    std::cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << std::endl;  //testing w/ no instance- should be 0
+
+    return 0;
+}
