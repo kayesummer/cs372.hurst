f2ab784da724becd497e78e59866f003a82b2285
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-05 21:11:02 -0500

    Week 3 Commit 8

--- apps/bagtest.cpp
+++ apps/bagtest.cpp
@@ -16,29 +16,33 @@ int main()
 
     if (aBagOfIntegers.size() == 0)
     {
-        std::cout << "Congrats, your bag is currently empty!" << std::endl;
+        std::cout << "Congrats, your bag is currently empty!" << std::endl; //constructor behavior
     }
     else
     {
         std::cout << "Opps, something went wrong and your bag is not empty!" << std::endl;
     }
 
-    for (int count = 0; count > 10; count++)    //testing insert function
+    for (int count = 0; count > 10; count++)    //Insert behavior
     {
         aBagOfIntegers.insert(count);
     }
-    aBagOfIntegers.print();     //testing print function
-
-    aBagOfIntegers.remove(9);   //testing remove function by removing the number 9
-    aBagOfIntegers.print();
-
-    std::cout << "The Size of the Bag: " << aBagOfIntegers.size() << std::endl;      //testing size function
-
-    for (int count = 0; count > 3; count++)     //testing count function
+    if (aBagOfIntegers.size() == 10)
     {
-        aBagOfIntegers.insert(2);
+        std::cout << "There are now 10 items in your bag." << std::endl;   
     }
-    std::cout << aBagOfIntegers.count() << " "<< std::endl;
+    else 
+    {
+        std::cout << "Something went wrong in the insert.() function!" << std::endl;
+    }
+
+
+    std::cout << "The Size of the Bag: " << aBagOfIntegers.size() << std::endl;      //size behavior
+    
+    aBagOfIntegers.insert(9);                                                                               //count behavior
+    std::cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << std::endl;     //testing w/ duplicate- should be 2
+    std::cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << std::endl;    //testing w/o duplicate- should be 1
+    std::cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << std::endl;  //testing w/ no instance- should be 0
 
     return 0;
 }
