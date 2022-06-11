44b6e911f0e30944a3aa7689d90cc515eb29a274
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-11 14:40:45 -0500

    Week 4 Commit 3

--- apps/receiptbagtest/receiptbagtest.cpp
+++ apps/receiptbagtest/receiptbagtest.cpp
@@ -7,53 +7,59 @@
 
 #include <iostream> 
 #include "receiptbag.h"
+using namespace std;
 
 
 
 int main()
 {
     ReceiptBag<int>aBagOfIntegers;
-    int uniqueReceipt = 0;
 
+    //constructor behavior
     if (aBagOfIntegers.size() == 0)
     {
-        std::cout << "Congrats, your bag is currently empty!" << std::endl; //constructor behavior
+        cout << "Congrats, your bag is currently empty!" << endl ; 
     }
     else
     {
-        std::cout << "Opps, something went wrong and your bag is not empty!" << std::endl;
+        cout << "Opps, something went wrong and your bag is not empty!" << endl ;
     }
 
-    for (int count = 0; count > 10; count++)    //Insert behavior
+    //Insert behavior
+    for (int count = 0; count > 10; count++)    
     {
-        aBagOfIntegers.insert(count);
+        aBagOfIntegers.insert(count) ;
     }
     if (aBagOfIntegers.size() == 10)
     {
-        std::cout << "There are now 10 items in your bag." << std::endl;   
+        cout << "There are now 10 items in your bag." << endl ;   
     }
     else 
     {
-        std::cout << "Something went wrong in the .insert() function!" << std::endl;
+        cout << "Something went wrong in the .insert() function!" << endl ;
     }
 
-
-    std::cout << "The Size of the Bag: " << aBagOfIntegers.size() << std::endl;      //size behavior
+    //size behavior
+    cout << "The Size of the Bag: " << aBagOfIntegers.size() << endl ;      
     
-    std:: cout << uniqueReceipt = aBagOfIntegers.insert(9) << std::endl;     //testing receipt return         //count behavior
-    std::cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << std::endl;     //testing w/ duplicate- should be 2
-    std::cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << std::endl;    //testing w/o duplicate- should be 1
-    std::cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << std::endl;  //testing w/ no instance- should be 0
+    //testing receipt return
+    cout << aBagOfIntegers.insert(9) << endl ;              
 
-    aBagOfIntegers.pop();       //pop behavior
+    //count behavior
+    cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << endl ;     //testing w/ duplicate- should be 2
+    cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << endl ;    //testing w/o duplicate- should be 1
+    cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << endl ;  //testing w/ no instance- should be 0
+
+    //pop behavior
+    aBagOfIntegers.pop();       
     if (aBagOfIntegers.size() == 10)
     {
-        std::cout << "There are 10 items in your bag, again." << std::endl;   
+        cout << "There are 10 items in your bag, again." << endl ;   
     }
     else 
     {
-        std::cout << "Something went wrong in the .pop() function!" << std::endl;
+        cout << "Something went wrong in the .pop() function!" << endl ;
     }
 
-    return 0;
+    return 0 ;
 }
