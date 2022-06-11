22a2034ef07b4bd960736e6a1dd8101287cd2030
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-06-11 15:35:00 -0500

    Week 4 Commit 8

--- apps/receiptbagtest/receiptbagtest.cpp
+++ apps/receiptbagtest/receiptbagtest.cpp
@@ -18,48 +18,48 @@ int main()
     //constructor behavior
     if (aBagOfIntegers.size() == 0)
     {
-        cout << "Congrats, your bag is currently empty!" << endl ; 
+        cout << "Congrats, your bag is currently empty!" << endl; 
     }
     else
     {
-        cout << "Opps, something went wrong and your bag is not empty!" << endl ;
+        cout << "Opps, something went wrong and your bag is not empty!" << endl;
     }
 
     //Insert behavior
     for (int count = 0; count > 10; count++)    
     {
-        aBagOfIntegers.insert(count) ;
+        aBagOfIntegers.insert(count);
     }
     if (aBagOfIntegers.size() == 10)
     {
-        cout << "There are now 10 items in your bag." << endl ;   
+        cout << "There are now 10 items in your bag." << endl;   
     }
     else 
     {
-        cout << "Something went wrong in the .insert() function!" << endl ;
+        cout << "Something went wrong in the .insert() function!" << endl;
     }
 
     //size behavior
-    cout << "The Size of the Bag: " << aBagOfIntegers.size() << endl ;      
+    cout << "The Size of the Bag: " << aBagOfIntegers.size() << endl;      
     
     //testing receipt return
     cout << aBagOfIntegers.insert(9) << endl ;              
 
     //count behavior
-    cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << endl ;     //testing w/ duplicate- should be 2
-    cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << endl ;    //testing w/o duplicate- should be 1
-    cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << endl ;  //testing w/ no instance- should be 0
+    cout << "The number '9' is in this list " << aBagOfIntegers.count(9) << " times." << endl;     //testing w/ duplicate- should be 2
+    cout << "The number '1' is in this list " << aBagOfIntegers.count(1) << " times." << endl;    //testing w/o duplicate- should be 1
+    cout << "The number '25' is in this list " << aBagOfIntegers.count(25) << " times." << endl;  //testing w/ no instance- should be 0
 
     //pop behavior
     aBagOfIntegers.pop();       
     if (aBagOfIntegers.size() == 10)
     {
-        cout << "There are 10 items in your bag, again." << endl ;   
+        cout << "There are 10 items in your bag, again." << endl;   
     }
     else 
     {
-        cout << "Something went wrong in the .pop() function!" << endl ;
+        cout << "Something went wrong in the .pop() function!" << endl;
     }
 
-    return 0 ;
+    return 0;
 }
