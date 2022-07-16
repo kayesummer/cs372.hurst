1ce869f0efb420d650e840fd3355502be3314eca
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-07-16 13:51:04 -0500

    Week 9 Commit 4

---
+++ include/structure.h
@@ -0,0 +1,21 @@
+//
+// File:    structure.h
+// Author: Kaylee Hurst
+// Purpose:
+// Implement ADT structure
+//
+
+#include "tree.h"
+#include <vector>
+#include <algorithms>
+
+class Structure
+{
+    private:
+        size_t size;
+
+
+    public:
+
+
+};
--- include/tree.h
+++ include/tree.h
@@ -24,30 +24,73 @@ class Tree
     public:
         Tree() {}   //default constructor
         Tree(Tree const &lft, V value, Tree const &rgt) :   //constructor
-            root(Node(val, lft->root, rgt->root)) {}
-        bool isEmpty() const
+            root(Node(v->value, lft->root, rgt->root)) {}
+        
+        Tree Tree::insert(V x) const  
+        {
+            if (isEmpty())
+            {
+                return Tree(Tree(), x, Tree());
+            }
+            V y = root();
+            if(x < y)
+            {
+                return Tree(left().insert(x), y, right());
+            }
+            else if (y < x)
+            {
+                return Tree(left(), y, right().insert(x));
+            }
+            else
+            {
+                return *this;   //no duplicates
+            }
+        }
+
+        bool isEmpty() const 
         {
 
         }
-        V root() const
+
+        V root() const 
+        {
+            return root;
+        }
+
+        Tree &left() const  
+        {
+            return left;
+        }
+
+        Tree &right() const  
+        {
+            return right;
+        }
+
+        bool member(V x) const   
+        {
+            if (isEmpty())
+            {
+                return false;
+            }
+            V y = root();
+            if (x < y)
+            {
+                return left().member(x);
+            }
+            else if (y < x)
+            {
+                return right.().member(x);
+            }
+            else 
+            {
+                return true;
+            }
+        }
+
+        bool isLeaf() const 
         {
 
-        }
-        Tree &left() const
-        {
-
-        }
-        Tree &right() const
-        {
-
-        }
-        bool member(V x) const
-        {
-
-        }
-        bool isLeaf() const
-        {
-            
         }
 
 
