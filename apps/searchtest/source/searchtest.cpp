ac721fee5d4af22b9fdb778cf85eb5f260d4e4ab
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-07-16 13:51:58 -0500

    Week 9 Commit 5

---
+++ apps/searchtest/source/searchtest.cpp
@@ -0,0 +1,17 @@
+//
+// File:    searchtest
+// Author: Kaylee Hurst
+// Purpose:
+// compares the execution time of binary search vs. lookup times in a binary search tree
+//
+
+#include <chrono>
+#include <iostream>
+#include "structure.h"
+
+int main()
+{
+
+
+    return 0;
+}
